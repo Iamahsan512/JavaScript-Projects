d3b79fd02821f396920d5150d81d67343e29d5e6
Ahsan Mughal <92372278+Iamahsan512@users.noreply.github.com>
2022-08-20 21:43:12 +0500

    first commit

---
+++ Random ColorCode generate/main.js
@@ -0,0 +1,38 @@
+'use strict';
+// Constant and variables 
+const containerEl = document.querySelector(".container");
+
+// Create Element div 
+for (let i = 0; i < 48; i++) {
+    const colorContainerEl = document.createElement("div");
+    colorContainerEl.classList.add("color-container");
+    containerEl.appendChild(colorContainerEl);
+}
+
+// Generate random color code 
+const randomColor = () => {
+    const chars = "0123456789abcdef";
+    const colorCodeLength = 6;
+    let colorCode = "";
+
+    for (let i = 0; i < colorCodeLength; i++) {
+        const randomNum = Math.floor(Math.random() * chars.length);
+        colorCode += chars.substring(randomNum, randomNum + 1);
+        // console.log(colorCode);
+    }
+    return colorCode;
+}
+
+const colorContainerEls = document.querySelectorAll(".color-container");
+
+
+// Styling of each div with color code 
+const generateColors = () => {
+    colorContainerEls.forEach((color) => {
+        const newColorCode = randomColor();
+        color.style.backgroundColor = "#" + newColorCode;
+        color.innerText = "#" + newColorCode;
+    });
+}
+
+generateColors();
