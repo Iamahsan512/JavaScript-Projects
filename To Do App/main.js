d1cd1a81b6fb8f5e107c627cde46cf35708fb167
Ahsan Mughal <92372278+ahsanwebengr@users.noreply.github.com>
2022-12-31 00:23:15 +0500

    Update main.js

--- To Do App/main.js
+++ To Do App/main.js
@@ -1,64 +1,83 @@
 'use strict';
-
+// Global Variables
 const input = document.querySelector('#input');
 const btnAdd = document.querySelector('#btn');
 const task = document.querySelector('.task');
+let data = [];
+// To store data in the screen after reloading
 
+if (localStorage.getItem('data') != null) {
+  data = JSON.parse(localStorage.getItem('data'));
+}
+// Add item in Todo buy clicking add btn
+btnAdd.addEventListener('click', () => {
+  const inputValue = input.value;
+  if (inputValue !== '') {
+    swal('Great work!', 'You added an item!', 'success');
+    getData();
+    createElement();
 
-input.addEventListener('keyup', function (e) {
-    // console.log(e.target.value);
-    const inputValue = e.target.value;
-
-    // disable button if zero value is entered 
-
-    if (inputValue === '') {
-        btnAdd.classList.add('disable');
-    }
-    else {
-        btnAdd.classList.remove('disable');
-    }
+    input.value = '';
+  } else {
+    swal('Please enter an item!', "You can't add empty field!", 'error');
+  }
 });
 
-// when add a new item 
+// Create new Element after adding every item
+const createElement = () => {
+  task.innerHTML = '';
+  data.forEach((val) => {
+    task.innerHTML += `
+                <div class="item">
+                  <p class="para">${val}</p>
+                  <div class="item_btn">
+                    <i class="fas fa-edit"></i>
+                    <i class="fas fa-trash"></i>
+                  </div>
+                </div>
+        `;
+  });
 
-btnAdd.addEventListener('click', function () {
-    // console.log(input.value);
-    const elem = input.value;
+  // !Delete the single item from todo
+  const delElem = document.querySelectorAll('.fa-trash');
+  delElem.forEach((del, index) => {
+    del.addEventListener('click', () => {
+      swal({
+        title: 'Are you sure?',
+        text: 'Once deleted, you will not be able to recover this imaginary file!',
+        icon: 'warning',
+        buttons: true,
+        dangerMode: true,
+      }).then((willDelete) => {
+        if (willDelete) {
+          swal('Poof! Your imaginary file has been deleted!', {
+            icon: 'success',
+          });
+          // !Delete the current target task
+          const taskItem = del.parentElement.parentElement;
+          taskItem.remove();
+          data.splice(index, 1);
+          localStorage.setItem('data', JSON.stringify(data));
+        } else {
+          swal('Your imaginary file is safe!');
+        }
+      });
+    });
+  });
 
-    if (elem.trim() !== '') {
-        let newItem = document.createElement('div');
-        newItem.classList.add('item');
-        newItem.innerHTML = `<p class="p"> ${elem}</p>
-        <div class="item_btn">
-          <i class="fas fa-edit"></i>
-          <i class="fas fa-times"></i>
-        </div>`
-        // Add one more child 
-        task.appendChild(newItem);
+  // !Done the single item from todo
+  const editElem = document.querySelectorAll('.fa-edit');
+  editElem.forEach((edit) => {
+    edit.addEventListener('click', () => {
+        const para = edit.parentElement.parentElement.childNodes[1];
+        para.classList.toggle('complete')
+    });
+  });
+};
+// get data and set into localStorage
+const getData = () => {
+  data.push(input.value);
+  localStorage.setItem('data', JSON.stringify(data));
+};
 
-        input.value = ""
-    }
-    else {
-        alert('Please enter a task');
-    }
-});
-
-// When delete an task 
-task.addEventListener('click', function (e) {
-    // console.log(e.target);
-
-    if (e.target.classList.contains('fa-times')) {
-        e.target.parentElement.parentElement.remove();
-        console.log(e.target.parentElement.parentElement);
-    }
-});
-
-// when complete an task 
-
-task.addEventListener('click', function (e) {
-    if (e.target.classList.contains('fa-edit')) {
-        e.target.parentElement.previousElementSibling.classList.toggle('complete');
-    }
-})
-
-
+createElement();
